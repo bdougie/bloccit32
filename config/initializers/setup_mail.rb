70102fecb1b74e4c17e0e4c6d89047d9f7811dbc
Brianllamar <ilikerobot@gmail.com>
2013-12-31 15:27:22 -0500

    fixed the error, sendgrid password was incorrect

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,7 +5,7 @@ if Rails.env.development?
     :port           => '587',
     :authentication => :plain,
     :user_name      => "app20785710@heroku.com",
-    :password       => "yyk6piczl",
+    :password       => "yk6piczl",
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
