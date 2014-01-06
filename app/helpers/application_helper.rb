35407dafc7dfb296213e44eea08680df1c345dbb
Brianllamar <ilikerobot@gmail.com>
2014-01-06 18:10:46 -0500

    Installed and configured pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
 		redcarpet = Redcarpet::Markdown.new(renderer, extensions)
 		(redcarpet.render text).html_safe
 	end
-=begin
+
 	def will_paginate(collection_or_options = nil, options = {})
 		if collection_or_options.is_a? Hash
       options, collection_or_options = collection_or_options, nil
@@ -23,5 +23,5 @@ module ApplicationHelper
     end
     super *[collection_or_options, options].compact
 	end
-=end
+
 end
--- config/initializers/bootstrap_link_renderer.rb
+++ config/initializers/bootstrap_link_renderer.rb
@@ -1,10 +1,29 @@
+class BootstrapLinkRenderer < WillPaginate::ActionView::LinkRenderer
+  def html_container(html)
+    tag :div, tag(:ul, html), container_attributes
+  end
 
-=begin
-<div class="pagination">
-  <ul>
-    <li class="disabled"><a href="#">&laquo;</a></li>
-    <li class="active"><a href="#">1</a></li>
-    ...
-  </ul>
-</div>
-=end
+  def page_number(page)
+    tag :li, 
+      link(page, page, rel: rel_value(page)), 
+      class: ('active' if page == current_page)
+  end
+
+  def previous_page
+    num = @collection.current_page > 1 && @collection.current_page - 1
+    previous_or_next_page(num, '&laquo;', 'previous_page')
+  end
+
+  def next_page
+    num = @collection.current_page < total_pages && @collection.current_page + 1
+    previous_or_next_page(num, '&raquo;', 'next_page')
+  end
+
+  def previous_or_next_page(page, text, classname)
+    tag :li, link(text, page || '#')
+  end
+
+  def gap
+    tag :li, link(super, '#'), class: 'disabled'
+  end
+end
