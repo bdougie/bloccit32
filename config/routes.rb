310214db55bf5193ac2b023241936daa4020b70f
Brianllamar <ilikerobot@gmail.com>
2014-01-12 18:36:07 -0500

    Created Popular Posts and Users, and optimized topics#show with eager loading.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Bloccit32::Application.routes.draw do
 
+  get "posts/index"
+
 devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' , registrations: 'users/registrations' }
 
 resources :users, only: [:show]
 
-
+  resources :users, only: [:show, :index]
+  resources :posts, only: [:index]
+  
   resources :topics do
-  	resources :posts, except: [:index] do # I missed adding the do here
+  	resources :posts, except: [:index] , controller: 'topics/posts' do # I missed adding the do here
 	    resources :comments, only: [:create, :destroy]
 	    match '/up-vote', to: 'votes#up_vote', as: :up_vote
       match '/down-vote', to: 'votes#down_vote', as: :down_vote
       resources :favorites, only: [:create, :destroy]
     end
 	end
-#uninitialized error constant CommentsHelper
+
 
 
   match "about" => 'welcome#about', via: :get
